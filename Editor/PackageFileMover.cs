5fd9181f010352d9d20deb6773ed0403a31eecaf
munteanuion <71140604+munteanuion@users.noreply.github.com>
2024-10-20 01:19:17 +0300

    Update PackageFileMover.cs

--- Editor/PackageFileMover.cs
+++ Editor/PackageFileMover.cs
@@ -20,7 +20,7 @@ public class PackageFileMover : AssetPostprocessor
             }
         }
 
-        AddDefineSymbol();
+        //AddDefineSymbol();
 
         void AddDefineSymbol()
         {
